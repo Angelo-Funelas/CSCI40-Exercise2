a311e87dfb79fe312e03987f7949a12914788da6
Angelo-Funelas <angelomartinfunelas@gmail.com>
2025-01-30 21:06:39 +0800

    Added handler for general cases

--- Fraction.py
+++ Fraction.py
@@ -7,9 +7,11 @@ class Fraction(object):
     def gcd(a, b):
         if a == 0 or b == 0:
             return 0
-        
         if a%b == 0 or b%a == 0:
             return min(a,b)
+        for num in range(min(a,b),0,-1):
+            if a%num == 0 and b%num == 0:
+                return num
         
 
     def get_numerator(self):
