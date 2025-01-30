340938ed27e06bacf5e5fee6eb27ba31b518287d
TwilightDC02 <justinbondc322@gmail.com>
2025-01-30 21:18:39 +0800

    Fixed spacing

--- Fraction.py
+++ Fraction.py
@@ -11,13 +11,14 @@ class Fraction(object):
         '''
         if a == 0 or b == 0:
             return 0
+        
         if a%b == 0 or b%a == 0:
             return min(a,b)
+        
         for num in range(min(a,b),0,-1):
             if a%num == 0 and b%num == 0:
                 return num
         
-
     def get_numerator(self):
         #TODO
         pass
